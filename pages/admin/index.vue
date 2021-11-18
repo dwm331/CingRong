99d5d82ca08f529d2492befd25a3245415d99b0c
Kathy Kuo <dudoo.kathy@gmail.com>
2021-11-18 15:41:25 +0800

    新增管理者頁面、登入窗口

---
+++ pages/admin/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="wrapper">
+    <Header></Header>
+    <div class="login_section">
+        <input type="text" class="form-control" placeholder="帳號" v-model="formEmail">
+        <input type="password" class="form-control" placeholder="密碼" v-model="formPassword">
+        <button class="btn btn-primary" type="button" @click="login">登入</button>
+    </div>
+  </div>
+</template>
+<script>
+import Header from "~/components/Header.vue";
+export default {
+  data() {
+    return {
+      formEmail: "",
+      formPassword: ""
+    }
+  },
+  methods: {
+    login() {
+    },
+  },
+};
+</script>
+<style>
+@import "~assets/css/components/header.min.css";
+@import "~assets/css/admin/style.min.css";
+</style>
+
