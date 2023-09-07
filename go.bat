6ec4904eeb929d3d233711ff9e35d6066d04f560
sam tu <khsam.sc.tu@foxconn.com>
2023-09-07 08:27:37 +0800

    調整bat 指令

--- go.bat
+++ go.bat
@@ -2,13 +2,4 @@
 echo 正在執行 git push
 git push
 
-echo 正在執行 yarn build:gh-pages
-yarn build:gh-pages
-
-echo 正在執行 yarn generate:gh-pages
-yarn generate:gh-pages
-
-echo 正在執行 yarn deploy
-yarn deploy
-
-echo 指令已完成執行
+echo 正在執行 yarn build:gh-pages && yarn build:gh-pages && echo 正在執行 yarn generate:gh-pages && yarn generate:gh-pages && echo 正在執行 yarn deploy && yarn deploy && echo 指令已完成執行
